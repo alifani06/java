bded3916b9403f1e3c0350b9d591497d426ddf02
Alifani <alifanimuhammad@gmail.com>
2024-09-04 09:13:43 +0700

    coba

--- resources/views/admin/laporan_deposit/print.blade.php
+++ resources/views/admin/laporan_deposit/print.blade.php
@@ -29,7 +29,7 @@
         }
     </style>
 </head>
-<body>
+{{-- <body>
 
     <h2 style="text-align: center; font-size: 28px;">LAPORAN DEPOSIT</h2>
     <div class="text-center">
@@ -101,24 +101,22 @@
                 <td style="text-align: right; border: none;">Total Deposit :</td>
                 <td style="text-align: right">{{ $totalDeposit }}</td>
 
-                {{-- <td style="text-align: right; font-weight: bold; border: none;">{{ number_format(, 0, ',', '.') }}</td> --}}
             </tr>
             <tr style="border: none;">
                 <td style="text-align: right; width: 60%; border: none;">Fee Deposit :</td>
                 <td style="text-align: right">{{ $totalFee }}</td>
 
-                {{-- <td style="text-align: right; font-weight: bold; width: 40%; border: none;">{{ number_format($, 0, ',', '.') }}</td> --}}
             </tr>
             <!-- Baris dengan garis pembatas -->
             <tr style="border-top: 2px solid black;">
                 <td style="text-align: right; border: none;">Sub Total  :</td>
                 <td style="text-align: right">{{ $subTotal }}</td>
 
-                {{-- <td style="text-align: right; font-weight: bold; border: none;">{{ number_format(, 0, ',', '.') }}</td> --}}
             </tr>
            
         </tbody>
     </table>
     
-</body>
+</body> --}}
+
 </html>
