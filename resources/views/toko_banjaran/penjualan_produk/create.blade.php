795d6dab2c2cabfeec682976c397fecc20ef72c2
Alifani <alifanimuhammad@gmail.com>
2024-09-11 08:17:38 +0700

    pdf

--- resources/views/toko_banjaran/penjualan_produk/create.blade.php
+++ resources/views/toko_banjaran/penjualan_produk/create.blade.php
@@ -449,7 +449,7 @@
         </div>
     </section>
   
-    
+  {{-- //pdf tab baru   --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
