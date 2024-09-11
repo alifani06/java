6b98fa30b625167be4e1f9da376a344cea74fba6
Alifani <alifanimuhammad@gmail.com>
2024-09-11 13:21:43 +0700

    deposit

--- resources/views/admin/laporan_deposit/index.blade.php
+++ resources/views/admin/laporan_deposit/index.blade.php
@@ -7,11 +7,11 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Laporan Data Deposit</h1>
+                    <h1 class="m-0">Laporan Data Deposit Global</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item active">Laporan Data Deposit</li>
+                        <li class="breadcrumb-item active">Laporan Data Deposit Global</li>
                     </ol>
                 </div>
             </div>
