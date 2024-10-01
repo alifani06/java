fb3abd5b0484ec3108e5aa00719a912f6c60889c
Alifani <alifanimuhammad@gmail.com>
2024-10-01 16:09:36 +0700

    fix

--- resources/views/admin/produk/qrcode_detail.blade.php
+++ resources/views/admin/produk/qrcode_detail.blade.php
@@ -95,12 +95,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Ban</h1>
+                            <h1 class="m-0">Produk</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item">
-                                    <a href="#">Ban</a>
+                                    <a href="#">Produk</a>
                                 </li>
                                 <li class="breadcrumb-item active">Lihat</li>
                             </ol>
