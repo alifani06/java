9b6c6462bd577cfffaec26ceb4f8b3add1a1e807
Alifani <alifanimuhammad@gmail.com>
2024-10-02 16:35:29 +0700

    tes

--- resources/views/admin/laporan_estimasiproduksi/print.blade.php
+++ resources/views/admin/laporan_estimasiproduksi/print.blade.php
@@ -91,7 +91,7 @@
         {{-- <p class="title1">Cabang: {{ strtoupper($selectedCabang) }}</p> --}}
         <div class="divider"></div>
 
-        <h1 class="title2">LAPORAN ESTIMASI ALL</h1>
+        <h1 class="title2">LAPORAN ESTIMASI GLOBAL</h1>
     
         @php
             use Carbon\Carbon;
