5f3295f5b2cbe9f30a03935cc180d4da68606b41
Alifani <alifanimuhammad@gmail.com>
2024-10-28 09:52:29 +0700

    s

--- resources/views/toko_bumiayu/index.blade.php
+++ resources/views/toko_bumiayu/index.blade.php
@@ -12,7 +12,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Cabang Tegal</li>
+                        <li class="breadcrumb-item active">Cabang Bumiayu</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
