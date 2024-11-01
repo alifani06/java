27cadf7ac8890e46326e50fc9528ad1116552327
Alifani <alifanimuhammad@gmail.com>
2024-11-01 12:41:53 +0700

    save

--- resources/views/toko_slawi/penjualan_produk/index.blade.php
+++ resources/views/toko_slawi/penjualan_produk/index.blade.php
@@ -107,8 +107,9 @@
                                         @endif
                                     </td>
                                     <td>
-                                        {{ $item->sub_total }}
+                                        {{ Str::startsWith($item->sub_total, 'Rp') ? $item->sub_total : 'Rp ' . number_format((float)$item->sub_total, 0, ',', '.') }}
                                     </td>
+                                    
                                     
 
                                     <td class="text-center">
