d78640156bb4fa718a3c15c34237a2400fab04b6
Alifani <alifanimuhammad@gmail.com>
2024-11-02 10:57:11 +0700

    s

--- resources/views/toko_pemalang/laporan_penjualanproduk/global.blade.php
+++ resources/views/toko_pemalang/laporan_penjualanproduk/global.blade.php
@@ -126,40 +126,42 @@
                         <tbody>
                             @php
                             $grandTotal = 0;
-                            $grandTotalFee = 0;
                         @endphp
-                            @foreach ($inquery as $item)
-                            @php
-                            $grandTotal += $item->sub_total;
-                 
-                            @endphp
-                                <tr class="dropdown"{{ $item->id }}>
-                                    <td class="text-center">{{ $loop->iteration }}</td>
-                                    <td>{{ $item->kode_penjualan }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($item->tanggal_penjualan)->format('d/m/Y H:i') }}</td>
-                                    {{-- <td>{{ $item->nama_pelanggan ?? 'Non Member' }}</td> --}}
-                                    <td>{{ $item->kasir}}</td>
-                                    <td>
-                                        @if ($item->detailpenjualanproduk->isNotEmpty())
-                                            {{ $item->detailpenjualanproduk->pluck('produk.klasifikasi.nama')->implode(', ') }}
-                                        @else
-                                            tidak ada
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if ($item->detailpenjualanproduk->isNotEmpty())
-                                            {{ $item->detailpenjualanproduk->pluck('nama_produk')->implode(', ') }}
-                                        @else
-                                            tidak ada
-                                        @endif
-                                    </td>
-                                    <td>{{ number_format($item->sub_total, 0, ',', '.') }}</td>
-                                </tr>
-                            @endforeach
-                            <tr>
-                                <td colspan="6" class="text-right"><strong>Grand Total</strong></td>
-                                <td>{{ 'Rp. ' .  number_format($grandTotal, 0, ',', '.') }}</td>
+                        @foreach ($inquery as $item)
+                        @php
+                            $sub_total = preg_replace('/[^\d]/', '', $item->sub_total);
+                            $sub_total = (float) $sub_total;
+                            $grandTotal += $sub_total; // Konversi ke float
+                        @endphp
+                            <tr class="dropdown"{{ $item->id }}>
+                                <td class="text-center">{{ $loop->iteration }}</td>
+                                <td>{{ $item->kode_penjualan }}</td>
+                                <td>{{ \Carbon\Carbon::parse($item->tanggal_penjualan)->format('d/m/Y H:i') }}</td>
+                                <td>{{ $item->kasir }}</td>
+                                <td>
+                                    @if ($item->detailpenjualanproduk->isNotEmpty())
+                                        {{ $item->detailpenjualanproduk->pluck('produk.klasifikasi.nama')->implode(', ') }}
+                                    @else
+                                        tidak ada
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($item->detailpenjualanproduk->isNotEmpty())
+                                        {{ $item->detailpenjualanproduk->pluck('nama_produk')->implode(', ') }}
+                                    @else
+                                        tidak ada
+                                    @endif
+                                </td>
+                                <td>
+                                    {{ Str::startsWith($item->sub_total, 'Rp') ? $item->sub_total : 'Rp ' . number_format((float)$item->sub_total, 0, ',', '.') }}
+                                </td> 
                             </tr>
+                        @endforeach
+                        <tr>
+                            <td colspan="6" class="text-right"><strong>Grand Total</strong></td>
+                            <td>{{ 'Rp. ' . number_format($grandTotal, 0, ',', '.') }}</td>
+                        </tr>
+                        
                         </tbody>
                     </table>
                     <!-- Modal Loading -->
