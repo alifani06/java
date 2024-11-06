1f900853900dbdfd2569b3b23ac5b9e75694d86e
Alifani <alifanimuhammad@gmail.com>
2024-11-06 07:14:35 +0700

    sss

--- resources/views/admin/inquery_pengirimanbarangjadi/cetak_barcode.blade.php
+++ resources/views/admin/inquery_pengirimanbarangjadi/cetak_barcode.blade.php
@@ -1,147 +1,7 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Produk</title>
-
-    <style type="text/css">
-        /* Reset all margins and padding */
-        * {
-            margin: 0;
-            padding: 0;
-        }
-
-        .box {
-            margin-left: 0px;
-            margin-top: 0px;
-        }
-        .text-container {
-            position: relative;
-            margin-right: 760px;
-            margin-left: 2px;
-            margin-top: 10px;
-            transform: rotate(90deg);   
-        }
-        .box1 {
-            margin-left: 0px;
-            margin-top: 40px;
-        }
-        .text-container1 {
-            position: relative;
-            margin-right: 760px;
-            margin-left: 2px;
-            margin-top: 10px;
-            transform: rotate(90deg);   
-        }
-        .box2 {
-            margin-left: 0px;
-            margin-top: 35px;
-        }
-        .text-container2 {
-            position: relative;
-            margin-right: 760px;
-            margin-left: 2px;
-            margin-top: 10px;
-            transform: rotate(90deg);   
-        }
-
-        .text {
-            font-size: 7px
-        }
-
-        .bold-text {
-            font-weight: bold;
-            font-family: Arial, Helvetica, sans-serif
-        }
-        .truncate {
-            width: 70px; /* Atur lebar sesuai kebutuhan */
-            overflow: hidden; /* Menyembunyikan teks yang melimpah */
-            white-space: nowrap; /* Mencegah teks untuk terbungkus */
-            text-overflow: ellipsis; /* Menampilkan elipsis jika teks terpotong */
-        }
-
-    </style>
-
-</head>
-
-<body>
-    <div>
-        <div class="box">
-            <?php
-            use BaconQrCode\Renderer\ImageRenderer;
-            use BaconQrCode\Writer;
-            
-            // Ubah tautan menjadi QR code
-            $qrcode = new Writer(new ImageRenderer(new \BaconQrCode\Renderer\RendererStyle\RendererStyle(50), new \BaconQrCode\Renderer\Image\SvgImageBackEnd()));
-            $qrcodeData = $qrcode->writeString($produk->qrcode_produk);
-            
-            // Tampilkan gambar QR code
-            echo '<img src="data:image/png;base64,' . base64_encode($qrcodeData) . '" />';
-            ?>
-        </div>
-        <div class="text-container">
-            <div class="text">
-                <p class="bold-text">{{ $produk->kode_lama }}</p>
-                <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
-                <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                <p class="bold-text">{{ $kodeProduksi ?? 'Kode produksi tidak tersedia' }}</p>
-            </div>
-        </div>
-
-        <div class="box1">
-            <?php
-            // Ubah tautan menjadi QR code
-            $qrcode = new Writer(new ImageRenderer(new \BaconQrCode\Renderer\RendererStyle\RendererStyle(50), new \BaconQrCode\Renderer\Image\SvgImageBackEnd()));
-            $qrcodeData = $qrcode->writeString($produk->qrcode_produk);
-            
-            // Tampilkan gambar QR code
-            echo '<img src="data:image/png;base64,' . base64_encode($qrcodeData) . '" />';
-            ?>
-        </div>
-        <div class="text-container1">
-            <div class="text">
-                <p class="bold-text">{{ $produk->kode_lama }}</p>
-                <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
-                <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                <p class="bold-text">{{ $kodeProduksi ?? 'Kode produksi tidak tersedia' }}</p>
-            </div>
-        </div>
-
-        <div class="box2">
-            <?php
-            // Ubah tautan menjadi QR code
-            $qrcode = new Writer(new ImageRenderer(new \BaconQrCode\Renderer\RendererStyle\RendererStyle(50), new \BaconQrCode\Renderer\Image\SvgImageBackEnd()));
-            $qrcodeData = $qrcode->writeString($produk->qrcode_produk);
-            
-            // Tampilkan gambar QR code
-            echo '<img src="data:image/png;base64,' . base64_encode($qrcodeData) . '" />';
-            ?>
-        </div>
-        <div class="text-container2">
-            <div class="text">
-                <p class="bold-text">{{ $produk->kode_lama }}</p>
-                <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
-                <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                <p class="bold-text">{{ $kodeProduksi ?? 'Kode produksi tidak tersedia' }}</p>
-            </div>
-        </div>
-
-      
-        
-    </div>
-</body>
-
-</html> --}}
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    {{-- <link rel="stylesheet" href="{{ asset('falcon/style.css') }}"> --}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -207,7 +67,7 @@
 
 </head>
 
-{{-- <body>
+<body>
     <div>
         <div class="box">
             <?php
@@ -227,7 +87,7 @@
                 <p class="bold-text">{{ $produk->kode_lama }}</p>
                 <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
                 <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                <p class="bold-text">{{ $kodeProduksi ?? '-' }}</p>
+                <p class="bold-text">{{ $kodeProduksi ?? 'Kode produksi tidak tersedia' }}</p>
             </div>
         </div>
 
@@ -246,7 +106,7 @@
                 <p class="bold-text">{{ $produk->kode_lama }}</p>
                 <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
                 <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                <p class="bold-text">{{ $kodeProduksi ?? '-' }}</p>
+                <p class="bold-text">{{ $kodeProduksi ?? 'Kode produksi tidak tersedia' }}</p>
             </div>
         </div>
 
@@ -265,61 +125,16 @@
                 <p class="bold-text">{{ $produk->kode_lama }}</p>
                 <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
                 <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                <p class="bold-text">{{ $kodeProduksi ?? '-' }}</p>
+                <p class="bold-text">{{ $kodeProduksi ?? 'Kode produksi tidak tersedia' }}</p>
             </div>
         </div>
 
       
         
     </div>
-</body> --}}
-
-<body>
-    <div>
-        @for ($i = 0; $i < $jumlah; $i += 3)
-            <div class="barcode-group">
-                <!-- Box 1 -->
-                <div class="box">
-                    <img src="data:image/png;base64,{{ $qrcodeData }}" />
-                    <div class="text-container">
-                        <div class="text">
-                            <p class="bold-text">{{ $produk->kode_lama }}</p>
-                            <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
-                            <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                            <p class="bold-text">{{ $kodeProduksi ?? '-' }}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Box 2 -->
-                <div class="box1">
-                    <img src="data:image/png;base64,{{ $qrcodeData }}" />
-                    <div class="text-container1">
-                        <div class="text">
-                            <p class="bold-text">{{ $produk->kode_lama }}</p>
-                            <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
-                            <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                            <p class="bold-text">{{ $kodeProduksi ?? '-' }}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Box 3 -->
-                <div class="box2">
-                    <img src="data:image/png;base64,{{ $qrcodeData }}" />
-                    <div class="text-container2">
-                        <div class="text">
-                            <p class="bold-text">{{ $produk->kode_lama }}</p>
-                            <p class="bold-text truncate">{{ $produk->nama_produk }}</p>
-                            <p style="font-size: 9px;" class="bold-text">Rp. {{ number_format($produk->harga, 0, ',', '.') }}</p>
-                            <p class="bold-text">{{ $kodeProduksi ?? '-' }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endfor
-    </div>
 </body>
 
-
 </html>
+
+
+
