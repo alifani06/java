2e2c0336a4ea1380bf233d43a9fbc36557f8df8b
Alifani <alifanimuhammad@gmail.com>
2024-11-06 10:11:35 +0700

    sss

--- resources/views/toko_slawi/penjualan_produk/cetak-pdf.blade.php
+++ resources/views/toko_slawi/penjualan_produk/cetak-pdf.blade.php
@@ -269,7 +269,7 @@
             <div class="text">
                 <h1>PT JAVA BAKERY FACTORY</h1>
                <p style="font-size: 10px;">Cabang : {{ $tokos->nama_toko }}</p>
-                <p style="font-size: 10px;">{{ $tokos->alamat }}</p>
+                <p style="font-size: 9.5px;">{{ $tokos->alamat }}</p>
             </div>
         </div>
         <hr class="divider">
