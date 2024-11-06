c1c950f1ccba3a791109896c220a773b8ca2b62d
Alifani <alifanimuhammad@gmail.com>
2024-11-06 09:47:34 +0700

    sss

--- resources/views/toko_banjaran/penjualan_produk/cetak-pdf.blade.php
+++ resources/views/toko_banjaran/penjualan_produk/cetak-pdf.blade.php
@@ -14,7 +14,7 @@
             margin-left: 0;
             margin-top: 0;
             /* padding: 0; */
-            padding-right: 460px;
+            padding-right: 465px;
             font-size: 10x;
             background-color: #fff;
         }
