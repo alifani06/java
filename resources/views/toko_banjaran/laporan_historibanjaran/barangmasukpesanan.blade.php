285cf0514f58014e5db9e34879697bb89f0b0041
Alifani <alifanimuhammad@gmail.com>
2024-11-30 11:37:31 +0700

    save bm banjaran pesanan

--- resources/views/toko_banjaran/laporan_historibanjaran/barangmasukpesanan.blade.php
+++ resources/views/toko_banjaran/laporan_historibanjaran/barangmasukpesanan.blade.php
@@ -94,13 +94,11 @@
                                 </select>
                             </div>
                             <div hidden class="col-md-3 mb-3">
-                                <select class="custom-select form-control" id="toko_id" name="toko_id">
-                                    <option value="">- Semua Toko -</option>
-                                    @foreach($tokos as $toko)
-                                        <option value="{{ $toko->id }}" {{ Request::get('toko_id') == $toko->id ? 'selected' : '' }}>{{ $toko->nama_toko }}</option>
-                                    @endforeach
-                                </select>
+                            
                                 <label for="toko_id">(Pilih Toko)</label>
+                                <select class="form-control" name="toko_id" id="toko_id" readonly>
+                                    <option value="1" selected>Toko Banjaran</option>
+                                </select>
                             </div>
                               
                             <div class="col-md-3 mb-3">
@@ -269,6 +267,7 @@
     function printReport() {
         var tanggalAwal = document.getElementById('tanggal_pengiriman').value;
         var tanggalAkhir = document.getElementById('tanggal_akhir').value;
+        var tokoId = document.getElementById('toko_id').value;
 
         if (tanggalAwal === "" || tanggalAkhir === "") {
             Swal.fire({
@@ -280,6 +279,21 @@
                 background: '#fff',
                 customClass: {
                     popup: 'animated bounceIn'
+                }
+            });
+            return;
+        }
+
+        if (tokoId != 1) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Toko Tidak Valid!',
+                text: 'Hanya bisa mencetak laporan untuk Toko ID 4.',
+                confirmButtonText: 'OK',
+                confirmButtonColor: '#d33',
+                background: '#fff',
+                customClass: {
+                    popup: 'animated shake'
                 }
             });
             return;
